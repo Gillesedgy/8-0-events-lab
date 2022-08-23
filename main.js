5cc0b6d78748a71cce2d04ff1a71c4f8660d8b37
Gillesedgy <edgygilles@pursuit.org>
2022-08-23 13:43:52 -0400

    Want to try Bonus

--- main.js
+++ main.js
@@ -6,4 +6,26 @@ for (let i = 0; i < 100; i++) {
   main.append(div);
 }
 
-// You may write your code here!
+//! You may write your code here!
+//*  When a color is clicked in the `#palette`, the `#current-color` element should have its background changed to that color. For example, if you click the green circle in the `#palette`, the `#current-color` element's background should change to be green.
+
+const color = document.querySelectorAll(".color");
+const current = document.querySelector("#current-color");
+
+color.forEach((cur) => {
+  cur.addEventListener("click", (event) => {
+    // console.log(event);
+    // console.log(color);
+  current.style.backgroundColor = event.target.style.backgroundColor
+  });
+});
+
+
+//* When you click on a `.cell`, its background should change to match the background of `#current-color`.
+const cell = document.querySelectorAll(".cell");
+
+cell.forEach((cel) => {
+  cel.addEventListener("click", () => {
+   cel.style.backgroundColor = current.style.backgroundColor
+  });
+});
